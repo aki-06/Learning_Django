68deb93dd1c7c7b639e361194ca8eaac6bcab130
akihironemoto <akihiro.nemoto.06@gmail.com>
2018-04-02 21:46:46 +0900

    template作成

--- first/myapp/views.py
+++ first/myapp/views.py
@@ -1,4 +1,8 @@
-from django.http import HttpResponse
+# from django.http import HttpResponse
+from django.shortcuts import render
 
 def index(request):
-	return HttpResponse('Hello, world!!')
+	context = {
+		'name': 'Aki'
+	}
+	return render(request, 'myapp/index.html', context)
