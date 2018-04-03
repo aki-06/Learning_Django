1bd4beef85ddb4eaee16aaf5b970735091435d82
akihironemoto <akihiro.nemoto.06@gmail.com>
2018-04-03 20:49:33 +0900

    fixed

--- first/myapp/views.py
+++ first/myapp/views.py
@@ -2,7 +2,22 @@
 from django.shortcuts import render
 
 def index(request):
+	"""
+	/トップページ
+	"""
 	context = {
 		'name': 'Aki'
 	}
 	return render(request, 'myapp/index.html', context)
+
+def about(request):
+	"""
+	/about
+	"""
+	return render(request, 'myapp/about.html')
+
+def info(request):
+	"""
+	/info
+	"""
+	return render(request, 'myapp/info.html')
